2d36c8bd674aa078272dc5bf7b86856cb5819b1f
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2025-11-06 22:16:59 -0500

    Codigo corregido, taller completado

--- src/main/java/ejercicio2/Main.java
+++ src/main/java/ejercicio2/Main.java
@@ -2,11 +2,11 @@ package ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
-        var documento = new Documento("La macarena");
-        documento.exportacionPDF();
-        var documentoExel = new ExportacionExel("Notas");
-        documentoExel.exportarExel();
-        var documentoWord = new ExportacionWord("Ensayo 2");
-        documentoWord.exportacionWord();
+        Documento documentoPDF = new Documento("La macarena");
+        documentoPDF.exportacion();
+        Documento documentoExel = new ExportacionExel("Notas");
+        documentoExel.exportacion();
+        Documento documentoWord = new ExportacionWord("Ensayo 2");
+        documentoWord.exportacion();
     }
 }
